72c5c92f34270554b893f04eaceeccfb1e18863d
Oleh Petraniuk <olehshort@gmail.com>
2019-11-15 01:18:13 +0100

    Add top menu stick function

--- js/main.js
+++ js/main.js
@@ -11,4 +11,18 @@
     });
   });
 
+  /*---------------------
+   TOP Menu Stick
+  --------------------- */
+  var s = $("#sticker");
+  var pos = s.position();
+  $(window).on('scroll', function() {
+    var windowpos = $(window).scrollTop() > 300;
+    if (windowpos > pos.top) {
+      s.addClass("stick");
+    } else {
+      s.removeClass("stick");
+    }
+  });
+
 });
