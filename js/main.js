f7d1d410f69ca206812c49e074f6084da802568a
Oleh Petraniuk <olehshort@gmail.com>
2019-11-15 01:23:22 +0100

    Add Testimonial function

--- js/main.js
+++ js/main.js
@@ -120,5 +120,27 @@
     $(this).addClass('active');
   });
 
+  /*---------------------
+   Testimonial carousel
+  ---------------------*/
+  var test_carousel = $('.testimonial-carousel');
+  test_carousel.owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+
 
 });
