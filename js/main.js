c59cf0c5858b2c52af0afaacf2dcaefb909601ff
Oleh Petraniuk <olehshort@gmail.com>
2019-11-15 01:19:29 +0100

    Add Navbar nav function

--- js/main.js
+++ js/main.js
@@ -25,4 +25,14 @@
     }
   });
 
+   /*----------------------------
+   Navbar nav
+  ------------------------------ */
+  var main_menu = $(".main-menu ul.navbar-nav li ");
+  main_menu.on('click', function() {
+    main_menu.removeClass("active");
+    $(this).addClass("active");
+  });
+
+
 });
