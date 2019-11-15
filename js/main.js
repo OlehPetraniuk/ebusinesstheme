12bcc4ed03be4e461f9b35fb19a49b9276373071
Oleh Petraniuk <olehshort@gmail.com>
2019-11-15 01:20:35 +0100

    Add slider function

--- js/main.js
+++ js/main.js
@@ -43,5 +43,22 @@
     $(".navbar-collapse.collapse").removeClass('in');
   });
 
+  //---------------------------------------------
+  //Nivo slider
+  //---------------------------------------------
+  $('#ensign-nivoslider').nivoSlider({
+    effect: 'random',
+    slices: 15,
+    boxCols: 12,
+    boxRows: 8,
+    animSpeed: 500,
+    pauseTime: 5000,
+    startSlide: 0,
+    directionNav: true,
+    controlNavThumbs: false,
+    pauseOnHover: true,
+    manualAdvance: false,
+  });
+
 
 });
